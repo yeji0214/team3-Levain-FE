3368c9d36e69861a56b0e3bf376bde300edef342
yzooop <qkrduswn0404@gmail.com>
2024-07-13 18:35:48 +0900

    💄 Main page

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,9 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Password from "./pages/Password";
+import Main from "./pages/Main";
 import MyPage from "./pages/MyPage";
+import LetterCreate from "./pages/LetterCreate";
 import NotFound from "./pages/NotFound";
 
 function App() {
@@ -11,7 +13,9 @@ function App() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/password" element={<Password />} />
+        <Route path="/main" element={<Main />} />
         <Route path="/letter/my" element={<MyPage />} />
+        <Route path="/letter/create" element={<LetterCreate />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
