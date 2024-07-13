61e4f53ebabe7f2b1bb609e7e2e203bc5674bf21
yzooop <qkrduswn0404@gmail.com>
2024-07-14 06:18:42 +0900

    ✨ intro page

--- src/App.jsx
+++ src/App.jsx
@@ -7,18 +7,20 @@ import OthersPage from "./pages/OthersPage";
 import NotFound from "./pages/NotFound";
 import LetterCreate from "./pages/LetterCreate";
 import Main from "./pages/Main";
+import Intro from "./pages/Intro";
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<LoginPage />} />
-          <Route path="/password" element={<Password />} />
-          <Route path="/main" element={<Main />} />
-          <Route path="/letter/my" element={<MyPage />} />
-          <Route path="/letter/:userName" element={<OthersPage />} />
-          <Route path="/letter/:userName/create" element={<LetterCreate />} />
-        <Route path="*" element={<NotFound />} />
+          <Routes>
+              <Route path="/" element={<Intro />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/password" element={<Password />} />
+            <Route path="/main" element={<Main />} />
+            <Route path="/letter/my" element={<MyPage />} />
+            <Route path="/letter/:userName" element={<OthersPage />} />
+            <Route path="/letter/:userName/create" element={<LetterCreate />} />
+            <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
   );
