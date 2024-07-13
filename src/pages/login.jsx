4e3237bb21965a17248fc3ba857b41f23497ba7a
yzooop <qkrduswn0404@gmail.com>
2024-07-13 16:18:06 +0900

    💄 login layout

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,16 +1,18 @@
-import { Link } from "react-router-dom"
-
-import { SubmitBtn } from "../components/Button"
-
-import Input from "../components/Input"
+import React from "react";
+import "../styles/pages/Login.css";
+import { Link } from "react-router-dom";
+import { SubmitBtn } from "../components/Button";
+import Input from "../components/Input";
+import bg1 from "../assets/bg1.png";
 
 function Login() {
     return (
         <div className="Login">
-            <form>
+            <div className="login-background" style={{ backgroundImage: `url(${bg1})` }}></div>
+            <form id="login-form">
                 <div className="login-title">로그인</div>
-                <div className="login-notice">초기 비밀번호는</div>
-                <div className="input-section">
+                <div className="login-notice">초기 비밀번호는 <strong>1234</strong> 입니다. <br /> 비밀번호 변경 후 로그인해주세요.</div>
+                <div className="login-input-section">
                     <Input
                         placeholder="아이디"
                         type="text"
@@ -20,13 +22,11 @@ function Login() {
                         type="password"
                     />
                 </div>
-
-                <SubmitBtn ButtonName="로그인" />
+                <SubmitBtn ButtonName="login" />
                 <Link to='/password'>비밀번호 변경</Link>
-
             </form>
         </div>
-    )
+    );
 }
 
-export default Login
+export default Login;
