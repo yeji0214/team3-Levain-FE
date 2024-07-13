c78de5740d1bbdf6879fb011bffde6887b2f5bda
yzooop <qkrduswn0404@gmail.com>
2024-07-13 14:31:44 +0900

    💄 login layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
-import NotFound from "./pages/NotFound";
+import Password from "./pages/Password";
 import MyPage from "./pages/MyPage";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/letter/my" element={<MyPage />} />
         <Route path="/" element={<Login />} />
+        <Route path="/password" element={<Password />} />
+        <Route path="/letter/my" element={<MyPage />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
--- src/pages/Password.jsx
+++ src/pages/Password.jsx
@@ -1,12 +1,10 @@
-import { Link } from "react-router-dom"
 import Input from "../components/Input"
 
-function Login() {
+function Password() {
     return (
         <div className="Login">
             <form>
-                <div className="login-title">로그인</div>
-                <div className="login-notice">초기 비밀번호는</div>
+                <div className="login-title">비밀번호 변경</div>
                 <div className="input-section">
                     <Input
                         placeholder="id"
@@ -16,11 +14,14 @@ function Login() {
                         placeholder="password"
                         type="password"
                     />
+                    <Input
+                        placeholder="password"
+                        type="password"
+                    />
                 </div>
-                <Link to='/password'>비밀번호 변경</Link>
             </form>
         </div>
     )
 }
 
-export default Login
+export default Password
